6f923db83a8a2a7f1fef5ed03e91b828b9f811c6
U-aero\shin <shin@aero.(none)>
2013-04-12 23:13:57 +0900

    Railsアプリケーションを新規に作成しました。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookshelf::Application.config.secret_token = 'dbf3d5e02619e1387a28916d0722f65cf5730d2cd3400fbb51937440e9c200c8ffae00c75380faee59ff48af4e3616ef967f2069ea7d2be701da227afe41e4dc'
