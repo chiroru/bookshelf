65a68c80c8c8a06a23d6952f79e8ecd5c6a2bef6
U-aero\shin <shin@aero.(none)>
2013-04-15 00:00:16 +0900

    add yui

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,11 @@
 class IndexController < ApplicationController
   def index
+    @shelves = Shelf.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @shelves }
+    end
   end
 
   def serach
